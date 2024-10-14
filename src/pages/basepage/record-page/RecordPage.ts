6653d3a0c9c97acdd03bb9715fc9ea2279419650
Marek Wiatroszak <marek.wiatroszak@gmail.com>
2024-10-14 21:56:24 +0200

    adding error

--- src/pages/basepage/record-page/RecordPage.ts
+++ src/pages/basepage/record-page/RecordPage.ts
@@ -2,6 +2,7 @@ import { expect, type Locator, type Page } from '@playwright/test';
 import { SObjectInterface } from '../../../api/sobjects/SobjectInterface';
 import { TabsSwitcher } from './TabsSwitcher';
 import { PlatformActionsComponent } from '../global/PlatformActionsComponent';
+import { log } from 'console';
 
 export class RecordPage<T extends SObjectInterface<T>>{
     readonly url: string = process.env.ORG_URL ? process.env.ORG_URL : '';
@@ -27,6 +28,7 @@ export class RecordPage<T extends SObjectInterface<T>>{
         return this
       }
     tabsStwitcher():TabsSwitcher<T> {
+        log
         return this.tabsSwitcher;
     }
     platformActions():PlatformActionsComponent{
