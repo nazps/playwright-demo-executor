97a22a27b695a11fc953ff788686e110f4bb1b8b
Marek Wiatroszak <marek.wiatroszak@gmail.com>
2024-10-14 10:36:06 +0200

    adding token to login to omit verification

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -5,11 +5,12 @@ import { LoginPage } from '../src/pages/login/LoginPage';
 const adminFile = 'playwright/.auth/admin.json';
 const username: string = process.env.USERNAME ? process.env.USERNAME : '';
 const password: string = process.env.PASSWORD ? process.env.PASSWORD : '';
+const token: string = process.env.TOKEN ? process.env.TOKEN : '';
 
 setup('authenticate as admin', async ({ page }) => {
     const loginPage = new LoginPage(page)
     await loginPage.goto();
-    const avatar = (await loginPage.login(username, password)).avatar;
+    const avatar = (await loginPage.login(username, password+token)).avatar;
     await expect(avatar).toHaveCSS('display', 'inline-block')
   await page.context().storageState({ path: adminFile });
 });
